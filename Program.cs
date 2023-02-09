6ecdf8987a7d6d74b7de40116ac14f35ee7bf692
alexs <sandroberidze65@gmail.com>
2023-02-10 01:16:14 +0400

    Some Refactor

--- Program.cs
+++ Program.cs
@@ -1,91 +1,50 @@
 ﻿using Installer;
 int selectedOption = 1;
+string option="";
 
 while (true)
 {
     Console.Clear();
 
-    if (selectedOption == 1)
-    {
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine("Node.JS");
-    }
-    else
-    {
-        Console.ForegroundColor = ConsoleColor.Gray;
-        Console.WriteLine("Node.JS");
-    }
+    Console.ForegroundColor = selectedOption == 1 ? ConsoleColor.Green : ConsoleColor.Gray;
+    Console.WriteLine("Node.JS");
 
-    if (selectedOption == 2)
-    {
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine("Visual Studio Code");
-    }
-    else
-    {
-        Console.ForegroundColor = ConsoleColor.Gray;
-        Console.WriteLine("Visual Studio Code");
-    }
+    Console.ForegroundColor = selectedOption == 2 ? ConsoleColor.Green : ConsoleColor.Gray;
+    Console.WriteLine("Visual Studio Code");
 
-    if (selectedOption == 3)
-    {
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine("Microsoft Visual Studio");
-    }
-    else
-    {
-        Console.ForegroundColor = ConsoleColor.Gray;
-        Console.WriteLine("Microsoft Visual Studio");
-    }
+    Console.ForegroundColor = selectedOption == 3 ? ConsoleColor.Green : ConsoleColor.Gray;
+    Console.WriteLine("Microsoft Visual Studio");
 
     Console.ForegroundColor = ConsoleColor.Gray;
     ConsoleKeyInfo pressedKey = Console.ReadKey();
+    Press(pressedKey, ref selectedOption);
+
+    if (pressedKey.Key == ConsoleKey.Enter)
+    {
+        switch (selectedOption)
+        {
+            case 1:
+                option = "Node.js";
+                Downloader.Download();
+                break;
+            case 2:
+                option = "Visual Studio Code";
+                break;
+            case 3:
+                option = "\"Microsoft Visual Studio\";";
+                break;
+        }
+        Console.WriteLine($"\n {option} Has Been Successfuly Installed ");
+        Console.ReadKey();
+    }
+}
+
+int Press(ConsoleKeyInfo pressedKey, ref int selectedOption)
+{
     if (pressedKey.Key == ConsoleKey.UpArrow)
-    {
-        if (selectedOption > 1)
-        {
-            selectedOption--;
-        }
-    }
+        selectedOption--;
     else if (pressedKey.Key == ConsoleKey.DownArrow)
-    {
-        if (selectedOption < 3)
-        {
-            selectedOption++;
-        }
-    }
-    if(selectedOption == 1)
-    {
-        var Option = "Node.js";
-
-         if (pressedKey.Key == ConsoleKey.Enter)
-        {
-            Downloader.Download();
-            Console.WriteLine($"\n {Option} Has Been Successfuly Installed ");
-            break;
-        }
-    }
-
-    if (selectedOption == 2)
-    {
-        string? gela = "Visual Studio Code";
-        if (pressedKey.Key == ConsoleKey.Enter)
-        {
-            Console.WriteLine("\nYou selected Option " + gela);
-            Console.ReadKey();
-            break;
-        }
-    }
-
-    if (selectedOption == 3)
-    {
-        string? gela = "Microsoft Visual Studio";
-
-        if (pressedKey.Key == ConsoleKey.Enter)
-        {
-            Console.WriteLine("\nYou selected Option " + gela);
-            Console.ReadKey();
-            break;
-        }
-    }
+        selectedOption++;
+    selectedOption = selectedOption < 1 ? 3 : selectedOption > 3 ? 1 : selectedOption;
+    return selectedOption;
 }
